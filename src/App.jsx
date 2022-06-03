98e69bb66979d7537689b2dd8cffb2861334cfde
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-06-03 17:45:41 +0545

    Navbar setup done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./pages/navbar/Navbar";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div>
-      <Home/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
