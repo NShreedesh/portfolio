db13b37bf6aa05499b7160217be2d38fdf7c3529
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-06-04 19:15:10 +0545

    Navbar Created.

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./navbar.scss";
 
 function Navbar() {
@@ -9,11 +9,21 @@ function Navbar() {
         <h1>My Portfolio</h1>
       </div>
       <div className="right-nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/project">Project</Link>
-        <Link to="/skills">Skills</Link>
-        <Link to="/work">Work</Link>
+        <NavLink to="/" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/about" activeClassName="active">
+          About
+        </NavLink>
+        <NavLink to="/project" activeClassName="active">
+          Project
+        </NavLink>
+        <NavLink to="/skills" activeClassName="active">
+          Skills
+        </NavLink>
+        <NavLink to="/work" activeClassName="active">
+          Work
+        </NavLink>
       </div>
     </nav>
   );
