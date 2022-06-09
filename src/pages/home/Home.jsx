7083563c02a87e9b103ba3408ed21afacb8caee8
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-06-09 13:59:49 +0545

    Progress on homepage ongoing.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,13 +4,19 @@ import "./home.scss";
 function Home() {
   return (
     <div id="home">
-      <div className="img-container">
-        <img
-          src={process.env.PUBLIC_URL + "/images/profile.jpg"}
-          alt="Profile"
-        />
+      <div className="body">
+        <div className="details">
+          <h1>Hi! I Am</h1>
+          <h1>Shreedesh Niroula</h1>
+          <p>A self taught and Strongly determined Game / Web developer</p>
+        </div>
+        <div className="img-container">
+          <img
+            src={process.env.PUBLIC_URL + "/images/profile.jpg"}
+            alt="Profile"
+          />
+        </div>
       </div>
-      <div></div>
     </div>
   );
 }
