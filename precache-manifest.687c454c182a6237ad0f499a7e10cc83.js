a7bc4b9ff77da419a9a1b2e34fd2c547a56f3010
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-06-18 07:52:37 +0545

    Updates

---
+++ precache-manifest.687c454c182a6237ad0f499a7e10cc83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "312f3805cec66bdb64da",
+    "url": "/static/css/main.a04ea097.chunk.css"
+  },
+  {
+    "revision": "312f3805cec66bdb64da",
+    "url": "/static/js/main.daee541f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8879e9366eb080c72c3",
+    "url": "/static/js/2.a32734e2.chunk.js"
+  },
+  {
+    "revision": "49ba16424bafb764ad73e1903ee58641",
+    "url": "/index.html"
+  }
+];
